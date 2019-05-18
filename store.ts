ec4e109fce5d536e14176a6f2e7dafc45e7cd0ae
matanbareketyoui <matan.bareket@youi.tv>
2019-05-17 22:18:41 -0400

    Switch from TSLint to ESLint

--- store.ts
+++ store.ts
@@ -10,9 +10,8 @@ import { applyMiddleware, createStore } from 'redux';
 import promise from 'redux-promise-middleware';
 import createDebounce from 'redux-debounced';
 import thunk from 'redux-thunk';
-
 import reducer from './reducers';
 
-const middleware = applyMiddleware(createDebounce(), thunk, promise());
+const middleware = applyMiddleware(createDebounce(), thunk, promise);
 
 export default createStore(reducer, middleware);
