bc6a615fb5db4e1ef04ab7a382b764de7f83b839
matanbareketyoui <matan.bareket@youi.tv>
2019-06-12 12:31:32 -0400

    Update to 5.3.0

--- rn-cli.config.js
+++ rn-cli.config.js
@@ -1,8 +1,14 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const blacklist = require('metro/src/blacklist');
+
 module.exports = {
   getTransformModulePath() {
     return require.resolve('react-native-typescript-transformer');
   },
   getSourceExts() {
     return ['ts', 'tsx'];
+  },
+  getBlacklistRE () {
+    return blacklist([/\/youi\/build\/.*/u]);
   },
 };
