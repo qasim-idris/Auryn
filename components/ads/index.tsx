892dbdbf20a182853086ca84ed2d31c5a820d64e
Erik Peña <erik.pena@youi.tv>
2019-11-15 16:36:26 -0800

    Implemented Ad Provider with Pause Ad - Ad provider will be responsible for controlling ads that are presented - Pause ad renders composition and manages show/hide actions

---
+++ components/ads/index.tsx
@@ -0,0 +1,66 @@
+import React, { createContext, createRef, RefObject, Component } from 'react';
+import { StyleSheet, View } from 'react-native';
+
+import PauseAd from './pause-ad';
+import { VideoContext } from './../videoPlayer/context';
+
+const END_SQUEEZE_MS = 15 * 1000;
+
+interface AdProviderProps {
+  pauseAdCompositionName: string | undefined,
+  onPauseAdClosed: () => void
+}
+
+interface AdProviderState {}
+
+const Context = createContext<AdProviderState>({});
+
+class AdProvider extends Component<AdProviderProps, AdProviderState> {
+  static defaultProps = {
+    pauseAdCompositionName: undefined,
+    related: [],
+    onPauseAdClosed: () => {}
+  };
+
+  static contextType = VideoContext;
+
+  private pausAdRef:RefObject<PauseAd> = createRef();
+
+  constructor(props:AdProviderProps) {
+    super(props);
+  }
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  render() {
+    const { children, pauseAdCompositionName, onPauseAdClosed } = this.props;
+    
+    return (
+      <Context.Provider value={this.state}>
+        <View style={styles.container}>
+          {pauseAdCompositionName !== undefined ?
+            <PauseAd
+              ref={this.pausAdRef}
+              name={pauseAdCompositionName}
+              forceShow={this.context.duration - this.context.currentTime < END_SQUEEZE_MS}
+              onClose={onPauseAdClosed}
+            /> :
+            null
+          }
+        </View>
+        {children}
+      </Context.Provider>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    ...StyleSheet.absoluteFillObject
+  }
+});
+
+export default AdProvider;
---
+++ components/ads/pause-ad.tsx
@@ -0,0 +1,89 @@
+import React, { Component, createRef, RefObject } from 'react';
+import { Composition, ButtonRef } from '@youi/react-native-youi';
+
+import { Timeline } from './../timeline';
+import { VideoContext } from './../videoPlayer/index';
+
+interface Props {
+  name: string,
+  forceShow: boolean,
+  onClose: () => void
+}
+
+interface State {
+  isShowing: boolean
+}
+
+class PauseAd extends Component<Props, State> {
+  static contextType = VideoContext;
+
+  static defaultProps = {
+    name: '',
+    forceShow: false
+  };
+
+  private adInTimeline:RefObject<Timeline> = createRef();
+  private adOutTimeline:RefObject<Timeline> = createRef();
+
+  constructor(props:Props) {
+    super(props);
+
+    this.state = { isShowing: false };
+  }
+
+  shouldComponentUpdate() {
+    return false;
+  }
+
+  componentDidUpdate() {
+    const { mediaState, playbackState, scrubbingEngaged } = this.context;
+
+    if(mediaState !== 'ready') return;
+
+    if(scrubbingEngaged) return;
+
+    if(playbackState === 'paused' || this.props.forceShow) {
+      this.showAd();
+    }
+
+    if(playbackState === 'playing' && !this.props.forceShow) {
+      this.hideAd();
+    }
+  }
+
+  public getIsShowing = () => this.state.isShowing;
+
+  public showAd = () => {
+    if(this.state.isShowing) return;
+    
+    this.setState({ isShowing: true });
+    this.adInTimeline.current?.play();
+  }
+
+  public hideAd = () => {
+    if(!this.state.isShowing) return;
+
+    this.setState({ isShowing: false });
+    this.adOutTimeline.current?.play();
+  }
+
+  onCloseClick = () => {
+    this.hideAd();
+    
+    this.props.onClose();
+  };
+
+  render() {
+    const { name } = this.props;
+
+    return (
+      <Composition source={name}>
+        <Timeline name="Animation" ref={this.adInTimeline} />
+        <Timeline name="Out" ref={this.adOutTimeline} />
+        <ButtonRef name="Btn-Cap" onPress={this.onCloseClick}/>
+      </Composition>
+    );
+  }
+}
+
+export default PauseAd;
