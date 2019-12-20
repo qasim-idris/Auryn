cdaa7f661119b7f15a7171caf97c30b07000e559
Matan Bareket <matan.bareket@youi.tv>
2019-12-20 16:35:18 -0500

    Add playOnChange and playOnTrue props to Timeline

--- src/components/timeline.tsx
+++ src/components/timeline.tsx
@@ -13,11 +13,14 @@ interface TimelineProps extends TimelineRefProps {
   onCompleted: () => void;
   name: string;
   autoplay: boolean;
+  playOnChange?: any;
+  playOnTrue?: boolean;
 }
 
 export class Timeline extends React.PureComponent<TimelineProps> {
   static defaultProps = {
     onCompleted: () => {},
+    onCompositionDidLoad: () => {},
     direction: 'forward',
     autoplay: false,
   };
@@ -27,8 +30,17 @@ export class Timeline extends React.PureComponent<TimelineProps> {
   resolve?: (value?: string) => void;
 
   componentDidMount() {
-    if (this.props.autoplay)
+    if (this.props.autoplay) this.play();
+  }
+
+  componentDidUpdate(prevProps: TimelineProps) {
+    if (this.props.playOnChange !== prevProps.playOnChange) {
       this.play();
+    }
+
+    if (this.props.playOnTrue && !prevProps.playOnTrue) {
+      this.play();
+    }
   }
 
   render() {
@@ -38,22 +50,21 @@ export class Timeline extends React.PureComponent<TimelineProps> {
         name={this.props.name}
         ref={this.innerRef}
         loop={this.props.loop || this.props.name.toLowerCase() === 'loop'}
+        onCompositionDidLoad={(ref) => this.props.onCompositionDidLoad(ref)}
         onCompleted={this.onCompleted}
       />
     );
   }
 
   play = (seek = 0) =>
-    new Promise(resolve => {
+    new Promise((resolve) => {
       this.resolve = resolve;
-      if (seek)
-        this.innerRef.current?.seek(this.props.direction === 'forward' ? seek : 1 - seek);
-      else
-        this.innerRef.current?.play();
+      if (seek) this.innerRef.current?.seek(this.props.direction === 'forward' ? seek : 1 - seek);
+      else this.innerRef.current?.play();
     });
 
   stop = () =>
-    new Promise(resolve => {
+    new Promise((resolve) => {
       this.resolve = resolve;
       this.innerRef.current?.stop();
     });
