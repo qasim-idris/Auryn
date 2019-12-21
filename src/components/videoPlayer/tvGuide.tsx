6ee2a7e49b1386a9664b0f0e308bbf82d4b007b1
Matan Bareket <matan.bareket@youi.tv>
2019-12-20 20:13:45 -0500

    Rename MiniGuide

---
+++ src/components/videoPlayer/tvGuide.tsx
@@ -0,0 +1,157 @@
+import React, { Fragment } from 'react';
+
+import { View, BackHandler } from 'react-native';
+import { ListRef, ListItem, ViewRef, FocusManager, ButtonRef, VideoUriSource } from '@youi/react-native-youi';
+import { connect } from 'react-redux';
+import { Timeline, LiveListItem } from '..';
+import { AurynAppState } from '../../reducers';
+import { ListItemPressEvent } from '../listitem';
+import { VideoContext, VideoContextType } from './context';
+import { Asset } from '../../adapters/asset';
+import { getDetailsByIdAndType } from '../../actions/tmdbActions';
+import { getVideoSourceByYoutubeId } from '../../actions/youtubeActions';
+import { AurynHelper } from '../../aurynHelper';
+
+type TvGuideDispatchProps = typeof mapDispatchToProps;
+
+interface TvGuideProps extends TvGuideDispatchProps {
+  liveData: Asset[];
+  asset: Asset;
+  visible: boolean;
+  onPressItem: ListItemPressEvent;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+const initialState = {
+  channelId: 'weather1',
+  isOpen: false,
+};
+
+class TvGuideComponent extends React.Component<TvGuideProps> {
+  declare context: React.ContextType<typeof VideoContext>;
+
+  static contextType = VideoContext;
+
+  tvGuideCloseButtonRef = React.createRef<ButtonRef>();
+
+  showGuideTimeline = React.createRef<Timeline>();
+
+  hideGuideTimeline = React.createRef<Timeline>();
+
+  firstListItem = React.createRef<LiveListItem>();
+
+  state = initialState;
+
+  componentDidMount() {
+    if (this.tvGuideCloseButtonRef.current) {
+      AurynHelper.togglePointerEvents(this.tvGuideCloseButtonRef, false);
+    }
+  }
+
+  componentWillUnmount() {
+    if (this.state.isOpen) this.hide();
+  }
+
+  navigateBack = () => {
+    if (this.state.isOpen) {
+      this.hide();
+      return true;
+    }
+
+    return false;
+  };
+
+  show = async () => {
+    if (this.state.isOpen)
+      return;
+
+    BackHandler.addEventListener('hardwareBackPress', this.navigateBack);
+    this.setState({ isOpen: true });
+    this.context.setTvGuideOpen(true);
+    AurynHelper.togglePointerEvents(this.tvGuideCloseButtonRef, true);
+    await this.showGuideTimeline.current?.play();
+    this.props.onOpen?.();
+    if (this.firstListItem.current) FocusManager.focus(this.firstListItem.current);
+  };
+
+  hide = async () => {
+    if (!this.state.isOpen)
+      return;
+
+    BackHandler.removeEventListener('hardwareBackPress', this.navigateBack);
+    this.props.onClose?.();
+    this.setState({ isOpen: false });
+    this.context.setTvGuideOpen(false);
+    AurynHelper.togglePointerEvents(this.tvGuideCloseButtonRef, false);
+    await this.hideGuideTimeline.current?.play();
+  };
+
+  openTvGuide = () => {
+    this.state.isOpen ? this.hide() : this.show();
+  };
+
+  onPressItem: ListItemPressEvent = (asset: Asset) => {
+    this.props.onPressItem?.(asset);
+
+    if (asset.id === this.props.asset.id) return;
+
+    const source =
+      asset.live?.streams[0].uri === this.context.videoSource?.uri ? asset.live?.streams[1] : asset.live?.streams[0];
+    this.props.getDetailsByIdAndType(asset.id, asset.type);
+    this.props.getVideoSourceByYoutubeId(asset.youtubeId);
+    this.context.setVideoSource(source as VideoUriSource);
+
+    this.hide();
+  };
+
+  renderLiveItem = ({ item, index }: ListItem<Asset>) => (
+    <View style={{ marginBottom: 20, marginTop: index === 0 ? 60 : 0 }}>
+      <LiveListItem
+        onPress={this.onPressItem}
+        data={item}
+        focusable={this.state.isOpen}
+        ref={index === 0 ? this.firstListItem : null}
+        shouldChangeFocus={false}
+      />
+    </View>
+  );
+
+  render() {
+    return (
+      <Fragment>
+        <ButtonRef
+          name="Btn-TvGuide"
+          onPress={this.openTvGuide}
+          visible={this.context.isLive}
+          focusable={!this.context.tvGuideOpen}
+        />
+
+        <ViewRef name="Live-TvGuide" visible={this.context.isLive}>
+          <Timeline name="ShowGuide" ref={this.showGuideTimeline} />
+          <Timeline name="HideGuide" ref={this.hideGuideTimeline} />
+          <ListRef
+            name="Live-TvGuide-List"
+            data={this.props.liveData}
+            renderItem={this.renderLiveItem}
+            extraData={this.state.isOpen}
+          />
+          <ButtonRef name="Btn-TvGuide-Close" onPress={this.hide} ref={this.tvGuideCloseButtonRef} />
+        </ViewRef>
+      </Fragment>
+    );
+  }
+}
+
+const mapDispatchToProps = {
+  getDetailsByIdAndType,
+  getVideoSourceByYoutubeId,
+};
+
+const mapStateToProps = (store: AurynAppState) => ({
+  liveData: store.tmdbReducer.live.data,
+});
+
+export const TvGuide = connect(mapStateToProps, mapDispatchToProps, null, { forwardRef: true })(
+  TvGuideComponent as any,
+);
